1e071c425b442984a76d52308e04d03ded8d0858
lapinRagnar <lapinragnar@gmail.com>
2022-07-26 16:35:56 +0200

    api call - bind data - background if temp cold or warm

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <div id="app">
+  <div 
+    id="app" 
+    :class="typeof 
+    weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''" 
+    
+    >
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search ...">
+        <input type="text" class="search-bar" placeholder="Search ..." v-model="query" @keypress="fetchWeather">
         <div class="test" id="yes"></div>
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="date">26 juillet 2022</div>
-          <div class="location">Lyon, France</div>
+          <div class="date">{{ dateBuilder()}}</div>
+          <div class="location">{{weather.name}} - {{weather.sys.country}}</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">9°C</div>
-          <div class="weather">la pluie</div>
+          <div class="temp">{{ Math.round(weather.main.temp)}}</div>
+          <div class="weather">{{ weather.weather[0].main}}</div>
         </div>
       </div>
     </main>
@@ -32,7 +37,34 @@ export default {
   },
   data(){
     return {
-      api_key: env.apikey
+      api_key: env.apikey,
+      url_base: 'http://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather(e){
+      if (e.key == 'Enter'){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(response => response.json())
+          .then(this.setResults)
+      }
+    },
+    setResults(results){
+      this.weather = results
+    },
+    dateBuilder(){
+      let d = new Date()
+      let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+      let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+      let day = days[d.getDay()]
+      let date = d.getDate()
+      let month = months[d.getMonth()]
+      let year = d.getFullYear()
+
+      return `${day}-${date}-${month}-${year}`
     }
   }
 }
@@ -54,6 +86,10 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: .4s;
+}
+
+#app.warm{
+  background-image: url('./assets/warm-background.jpg');
 }
 
 main{
